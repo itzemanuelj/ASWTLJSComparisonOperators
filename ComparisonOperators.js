0b14729cc6e8636ec5ad8dc98204e1ac6a7783cd
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-28 00:57:45 -0400

    awnsers 1-10

--- ComparisonOperators.js
+++ ComparisonOperators.js
@@ -1,3 +1,55 @@
 // ===, !==, >, and <= are ________ operators.
 
 // comparison operators
+
+i != 1;
+
+// What is the comparison operator for is greater than?
+
+i > 1;
+
+// What is the comparison operator for is less than or equal to?
+
+i <= 1;
+
+// create this if statement: If a isn't equal to b, a is assigned the value of c.
+
+if (a != b) {
+    a = c;
+}
+
+// Code the first line of an if statement that tests whether num1 is greater than num2.
+
+if (num1 > num2) {
+    
+}
+
+// Code the first line of an if statement that tests whether num1 is greater than or equal to num2.
+
+if (num1 >= num2) {
+
+}
+
+// Code the first line of an if statement that tests whether one variable is unequal to another.
+
+if (a !== b)  {
+
+}
+
+// Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+
+if (hello != "hello") {
+
+}
+
+// Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+if (a < 2) {
+
+}
+
+// Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+
+if (a >= b) {
+
+}

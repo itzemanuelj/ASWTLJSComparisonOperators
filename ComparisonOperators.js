cbed0c9f68bc75739b45d74ca2ffa0ea3a8418e6
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-28 13:47:26 -0400

    mvp commot for comparsion operator

--- ComparisonOperators.js
+++ ComparisonOperators.js
@@ -53,3 +53,73 @@ if (a < 2) {
 if (a >= b) {
 
 }
+
+// Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+
+if ("same" !== same) {
+
+}
+
+// Time goal: 25 seconds. Code the first line of an if statement that tests whether one variable is unequal to another.
+
+if (a !== b){
+
+}
+
+// Time goal: 30 seconds. Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+
+if (a !== b) {
+
+}
+
+// Time goal: 25 seconds. Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+if (a < 1) {
+
+}
+
+// Time goal: 25 seconds. Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+
+if (num1 >= num2) {
+
+}
+
+// Time goal: 30 seconds. Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+
+if ("a" != num) {
+
+}
+
+// Time goal: 45 seconds. Code an if statement. Test whether a variable is unequal to a particular number. If so, assign a number to that variable.
+
+if (num != 1) {
+    num = 0;
+}
+
+// Code an if statement that tests whether a number is unequal to a different number.
+// If the condition is true (it will be), display a congratulations alert.
+// Click the Run It button to run your code live.
+
+if (1 != 2) {
+alert("Congrulations");
+}
+
+// Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered is unequal to another name.
+// If the condition is true (it will be), display an alert that says "No match"
+// Click the Run It button to run your code live.
+
+var name = prompt ("What is your name?");
+
+if (name != name1) {
+alert("No match");
+}
+
+// >= // greater than or equal 
+// <= // less than or equal
+// > // greater than
+// < // less than
+// == // equals
+// === // strictly equals
+// != // not equals
+// !== // stricly not equals
